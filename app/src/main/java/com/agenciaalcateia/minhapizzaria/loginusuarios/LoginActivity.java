8d8ddec9e015612145254ce44012f3df9a55dd3d
Alberto Zaranza <albertozaranza@outlook.com>
2018-07-13 12:41:31 -0300

    Criação do projeto

---
+++ app/src/main/java/com/agenciaalcateia/minhapizzaria/loginusuarios/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.agenciaalcateia.minhapizzaria.loginusuarios;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.agenciaalcateia.minhapizzaria.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
